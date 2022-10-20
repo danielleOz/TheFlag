1eccf195839448d547bb56c40d588d75748f55e7
ophirhose <ophir.hosy@gmail.com>
2022-10-20 16:58:09 +0300

    import pygame

--- MineField.py
+++ MineField.py
@@ -5,16 +5,22 @@ mine_field = []
 
 
 def is_empty(row, col):
+    for r in range(8):  # מקום של חייל שלא נמצא במטריצה
+        for c in range(2):
+            if row == r and col == c:
+                return False
+
     if mine_field[row][col] == Consts.EMPTY:
         return True
+
     return False
 
 
 def random_mine():
     mine_location = []
     for i in range(Consts.MINE_NUM):
-        row = random.randint(0, Consts.FIELD_ROWS)
-        col = random.randint(0, Consts.FIELD_COLS)
+        row = random.randrange(Consts.FIELD_ROWS)
+        col = random.randrange(Consts.FIELD_COLS)
         if is_empty(row, col):
             mine_location.append([row, col])
         else:
@@ -22,12 +28,9 @@ def random_mine():
     return mine_location
 
 
-
-
-
 def create():
     global mine_field
-    mine_field = [[Consts.EMPTY for i in range(Consts.FIELD_ROWS)] for j in range(Consts.FIELD_COLS)]
+    mine_field = [[Consts.EMPTY for i in range(Consts.FIELD_COLS)] for j in range(Consts.FIELD_ROWS)]
     mine_field[0][0] = Consts.SOLDIER
     for row in range(21, 23):
         for col in range(46, 49):
@@ -37,7 +40,7 @@ def create():
 
 def add_mines():
     for mine in random_mine():
-        mine_field[mine[0]][mine[1]]
+        mine_field[mine[0]][mine[1]] = Consts.MINE
 
 
 def move_soldier():
