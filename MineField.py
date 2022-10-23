9ff02476470e3b1777a0384f94da1d035547097e
noacholcman <noacholcman@gmail.com>
2022-10-23 16:36:19 +0300

    RANDOM_MINE

--- MineField.py
+++ MineField.py
@@ -4,24 +4,11 @@ import random
 mine_field = []
 
 
-def is_empty(row, col):
-    for r in range(4):  # מקום של חייל שלא נמצא במטריצה
-        for c in range(2):
-            if row == r and col == c:
-                return False
-    if col + 2 >= Consts.FIELD_COLS:
-        return False
-    for i in range(3):
-        if mine_field[row][col + i] != Consts.EMPTY:
-            return False
-    return True
-
-
 def create():
     global mine_field
     mine_field = [[Consts.EMPTY for i in range(Consts.FIELD_COLS)] for j in
                   range(Consts.FIELD_ROWS)]
-    mine_field[0][0] = Consts.SOLDIER
+    # mine_field[0][0] = Consts.SOLDIER
     for row in range(21, 24):
         for col in range(46, 50):
             mine_field[row][col] = Consts.FLAG
@@ -39,6 +26,19 @@ def add_mines():
         if is_empty(row, col):
             insert_mine(row, col)
             i += 1
+
+
+def is_empty(row, col):
+    for r in range(4):  # מקום של חייל שלא נמצא במטריצה
+        for c in range(2):
+            if row == r and col == c:
+                return False
+    if col + 2 >= Consts.FIELD_COLS:
+        return False
+    for i in range(3):
+        if mine_field[row][col + i] != Consts.EMPTY:
+            return False
+    return True
 
 
 def insert_mine(row, col):
@@ -66,28 +66,3 @@ def find_mines():
             else:
                 col += 1
     return mine_location
-
-
-def move_soldier(movement):
-    soldier_place = find_soldier()
-    row = soldier_place[0]
-    col = soldier_place[1]
-    if movement == Consts.DOWN and row + 4 < Consts.FIELD_ROWS:
-        mine_field[row + 1][col] = Consts.SOLDIER
-        mine_field[row][col] = Consts.EMPTY
-    if movement == Consts.UP and row - 1 > -1:
-        mine_field[row - 1][col] = Consts.SOLDIER
-        mine_field[row][col] = Consts.EMPTY
-    if movement == Consts.LEFT and col - 1 > -1:
-        mine_field[row][col - 1] = Consts.SOLDIER
-        mine_field[row][col] = Consts.EMPTY
-    if movement == Consts.RIGHT and col + 2 < Consts.FIELD_COLS:
-        mine_field[row][col + 1] = Consts.SOLDIER
-        mine_field[row][col] = Consts.EMPTY
-
-
-def find_soldier():
-    for row in range(Consts.FIELD_ROWS):
-        for col in range(Consts.FIELD_COLS):
-            if mine_field[row][col] == Consts.SOLDIER:
-                return [row, col]
--- Soldier.py
+++ Soldier.py
@@ -2,6 +2,35 @@ import Consts
 import MineField
 import main
 
+soldier_field = [[Consts.EMPTY for i in range(Consts.FIELD_COLS)] for j in
+                 range(Consts.FIELD_ROWS)]
+mine_field[0][0] = Consts.SOLDIER
+
+
+def find_soldier():
+    for row in range(Consts.FIELD_ROWS):
+        for col in range(Consts.FIELD_COLS):
+            if soldier_field[row][col] == Consts.SOLDIER:
+                return [row, col]
+
+
+def move_soldier(movement):
+    soldier_place = find_soldier()
+    row = soldier_place[0]
+    col = soldier_place[1]
+    if movement == Consts.DOWN and row + 4 < Consts.FIELD_ROWS:
+        soldier_field[row + 1][col] = Consts.SOLDIER
+        soldier_field[row][col] = Consts.EMPTY
+    if movement == Consts.UP and row - 1 > -1:
+        soldier_field[row - 1][col] = Consts.SOLDIER
+        soldier_field[row][col] = Consts.EMPTY
+    if movement == Consts.LEFT and col - 1 > -1:
+        soldier_field[row][col - 1] = Consts.SOLDIER
+        soldier_field[row][col] = Consts.EMPTY
+    if movement == Consts.RIGHT and col + 2 < Consts.FIELD_COLS:
+        soldier_field[row][col + 1] = Consts.SOLDIER
+        soldier_field[row][col] = Consts.EMPTY
+
 
 def is_lose():  # אם רגלי החייל נוגעות במקש
     soldier_sqr = MineField.find_soldier()
