2906353fe45827961f7febc099d3b009490c0eb0
Noya Bauer <noyab123@gmail.com>
2022-10-23 14:56:41 +0300

    ZZZ

--- Consts.py
+++ Consts.py
@@ -7,7 +7,7 @@ BACKGROUND_COLOR = GREEN
 SQUARE_SIZE = 25
 WINDOW_HEIGHT = FIELD_ROWS * SQUARE_SIZE
 WINDOW_WIDTH = FIELD_COLS * SQUARE_SIZE
-MINE_NUM = 20
+MINE_NUM = 20                              
 GRASS_NUM = 20
 EMPTY = "EMPTY"
 SOLDIER = "SOLDIER"
@@ -26,11 +26,11 @@ WIN_FONT_SIZE = LOSE_FONT_SIZE
 WIN_COLOR = (89, 89, 89)
 WIN_LOCATION = \
     (0.2 * WINDOW_WIDTH, WINDOW_HEIGHT / 2 - (WIN_FONT_SIZE / 2))
-FONT_NAME = "Calibri"
-TEXT_COLOR = BLACK
+FONT_NAME = "ariel"
+TEXT_COLOR = (255, 255, 255)
 TEXT_TEXT = f"Welcome to The Flag game. \n Have Fun!"
 TEXT_FONT_SIZE = 20
-TEXT_LOCATION = (10, WINDOW_HEIGHT - TEXT_FONT_SIZE - 10)
+TEXT_LOCATION = (20, 10)
 
 SOLDIER_IMG = "soldier.png"
 SOLDIER_HEIGHT = 4 * SQUARE_SIZE
@@ -53,8 +53,8 @@ FLAG_X = WINDOW_WIDTH / 2 - (FLAG_WIDTH / 2)
 FLAG_Y = WINDOW_HEIGHT * 0.8
 
 MINE_IMG = "mine.png"
-MINE_HEIGHT = 3 * SQUARE_SIZE
-MINE_WIDTH = SQUARE_SIZE
+MINE_HEIGHT = SQUARE_SIZE
+MINE_WIDTH = 3 * SQUARE_SIZE
 MINE_X = WINDOW_WIDTH / 2 - (MINE_WIDTH / 2)
 MINE_Y = WINDOW_HEIGHT * 0.8
 
@@ -67,18 +67,17 @@ UP = 'UP'
 LEFT = 'LEFT'
 RIGHT = 'RIGHT'
 
+GRID_LINE_COLOR = (0, 128, 0)
+
 
 def grass_loc():
     grass_locations = []
     for i in range(GRASS_NUM):
-        x = random.randint(0, FIELD_COLS)
-        y = random.randint(0, FIELD_ROWS)
+        x = random.randint(0, FIELD_COLS-3)
+        y = random.randint(0, FIELD_ROWS-3)
         loc = (x, y)
         grass_locations.append(loc)
     return grass_locations
 
 
 GRASS_LOCATIONS = grass_loc()
-
-
-
